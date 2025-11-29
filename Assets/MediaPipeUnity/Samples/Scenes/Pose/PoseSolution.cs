a9d2e4c1c6d2a78cb8942d975de5897921a8f6f2
Luis Guilherme Silva Rodrigues <guilherme.rodrigues@unesp.br>
2025-11-29 14:27:45 -0300

    Disabling legacy windows

--- Assets/MediaPipeUnity/Samples/Scenes/Pose/PoseSolution.cs
+++ Assets/MediaPipeUnity/Samples/Scenes/Pose/PoseSolution.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class PoseSolution : MonoBehaviour
 {
+  [SerializeField] private bool disableLegacyConfigWindows = true;
+  
   void Awake()
   {
-    transform.GetChild(0).GetComponent<Canvas>().worldCamera = Camera.main;
+    var mainCanvas = transform.GetChild(0);
+    mainCanvas.GetComponent<Canvas>().worldCamera = Camera.main;
+    if (disableLegacyConfigWindows)
+    {
+      var containerPanel = mainCanvas.GetChild(0);
+      var headerPanel = containerPanel.GetChild(1);
+      headerPanel.gameObject.SetActive(false);
+      var footerPanel = containerPanel.GetChild(2);
+      footerPanel.gameObject.SetActive(false);
+    }
   }
 }
